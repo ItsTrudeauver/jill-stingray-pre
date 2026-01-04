4e31ebf1907adba37c5a2119fbea6e8541c955fc
ItsTrudeauver <smz.osaragi@gmail.com>
2026-01-04 13:31:57 +0700

    debugged dashboard

--- utils/default.js
+++ utils/default.js
@@ -1,6 +1,23 @@
 module.exports = {
     // The "Factory Settings" for every server
     DEFAULT_RULES: {
+
+        "8ball": { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        avatar: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        ghost: { enabled: true, min_perm: "manageGuild", allow_channels: [], block_channels: [] },
+        id: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        menu: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        perms: { enabled: true, min_perm: "administrator", allow_channels: [], block_channels: [] },
+        ping: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        pulse: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        role: { enabled: true, min_perm: "manageRoles", allow_channels: [], block_channels: [] },
+        serverinfo: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        streamline: { enabled: true, min_perm: "manageMessages", allow_channels: [], block_channels: [] },
+        surprise: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        tab: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        threads: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        userinfo: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
+        
         // Public Commands
         mix: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
         custom: { enabled: true, min_perm: null, allow_channels: [], block_channels: [] },
